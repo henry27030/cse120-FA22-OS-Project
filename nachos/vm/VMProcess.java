f5464167a351467a2395e91a7507b35bbdf0459c
Henry Lam <h2lam@ieng6-203.ucsd.edu>
2022-11-18 14:52:26 -0800

    read/write account for invalid virtual pages now

--- nachos/vm/VMProcess.java
+++ nachos/vm/VMProcess.java
@@ -66,7 +66,8 @@ public class VMProcess extends UserProcess {
 			for (int i = 0; i < section.getLength(); i++) {
 				int vpn = section.getFirstVPN() + i;
         pageTable[vpn].readOnly = section.isReadOnly();
-        CoffSections[vpn]=s;//to match to the pageTable index to load in the case of a page fault
+        CoffSections[vpn]=s;// to match to the pageTable index that is set to readOnly to load in the case of a page fault
+        //set to s so that we can call loadPage later in a similar to way to initial implementation
       }
     }
 		return true;
@@ -93,21 +94,21 @@ public class VMProcess extends UserProcess {
     case Processor.exceptionPageFault:
       //prepare requested page
       int badVPN = Machine.processor().readRegister(Processor.regBadVAddr);
-      prepRequestedPage(badVPN/pageSize);//regBadVAddr gives you the bad VA register, so divide by pageSize to get vm index
+      prepRequestedPage(badVPN/pageSize);//regBadVAddr gives you the bad VA register, so divide by pageSize to get index, like in hw3
       break;
 		default:
 			super.handleException(cause);
 			break;
 		}
 	}
-  protected void prepRequestedPage (int badVPN) {
+  public void prepRequestedPage (int badVPN) {
     //pageTable[badVPN] is invalid
     //3 cases: fault on a code page, fault on a data page, fault on a stack page/args page
     //how to tell difference between these 3 pages?
     //first two cases, it is in the COFF file, and these files are readOnly true
     if (pageTable[badVPN].readOnly==true) {
-      CoffSection section = coff.getSection(CoffSections[badVPN]);
-      section.loadPage(badVPN-section.getFirstVPN(), pageTable[badVPN].ppn);//badVPN-section.getFirstVPN=i from loadSection's loadPage usage
+      CoffSection section = coff.getSection(CoffSections[badVPN]);//CoffSections[badVPN]=s
+      section.loadPage(badVPN-section.getFirstVPN(), pageTable[badVPN].ppn);//badVPN-section.getFirstVPN=i from UserProcess's loadPage(i, ppn)
     } 
     
     else {
@@ -116,6 +117,135 @@ public class VMProcess extends UserProcess {
     }
     pageTable[badVPN].valid=true;
   }
+	/**
+	 * Transfer data from this process's virtual memory to the specified array.
+	 * This method handles address translation details. This method must
+	 * <i>not</i> destroy the current process if an error occurs, but instead
+	 * should return the number of bytes successfully copied (or zero if no data
+	 * could be copied).
+	 *
+	 * @param vaddr the first byte of virtual memory to read.
+	 * @param data the array where the data will be stored.
+	 * @param offset the first byte to write in the array.
+	 * @param length the number of bytes to transfer from virtual memory to the
+	 * array.
+	 * @return the number of bytes successfully transferred.
+	 */
+	public int readVirtualMemory(int vaddr, byte[] data, int offset, int length) {
+/*
+		Lib.assertTrue(offset >= 0 && length >= 0
+				&& offset + length <= data.length);
+*/
+    //checking for valid virtual page conditions
+    if (!(offset >= 0 && length >= 0 && offset + length <= data.length) ||
+        data==null ||
+        vaddr<0 ||
+        vaddr+length>pageTable.length*pageSize) {
+      return -1;
+    }
+
+		byte[] memory = Machine.processor().getMemory();
+
+		// for now, just assume that virtual addresses equal physical addresses
+		if (vaddr < 0 || vaddr >= memory.length)
+			return 0;
+
+		int amount = Math.min(length, memory.length - vaddr);
+		//System.arraycopy(memory, vaddr, data, offset, amount);
+
+    int iterable = 0;
+    int page, diff;
+    do
+    {
+
+      page = (vaddr + iterable) / pageSize; //getting virtual page
+      diff = (vaddr + iterable) % pageSize; //getting offset within the page
+      
+      int spaceLeft = pageSize - diff; //the amount of space we can read from.
+      
+      //is the case where we have too much in our length to write
+      if(spaceLeft < length - iterable)
+      {
+        //System.out.println(iterable + " " + pageSize + " " + spaceLeft + " " + (length - iterable));
+        System.arraycopy(memory, pageTable[page].ppn*pageSize + diff, data, offset+iterable, spaceLeft);
+        iterable += spaceLeft;
+      }
+      else //is the case where we can write all that's left within the page
+      {
+        System.arraycopy(memory, pageTable[page].ppn*pageSize + diff, data, offset+iterable, length - iterable);
+        iterable += length - iterable;
+      }
+    }
+    while(iterable < length);
+
+		return iterable;
+	}
+
+	/**
+	 * Transfer data from the specified array to this process's virtual memory.
+	 * This method handles address translation details. This method must
+	 * <i>not</i> destroy the current process if an error occurs, but instead
+	 * should return the number of bytes successfully copied (or zero if no data
+	 * could be copied).
+	 *
+	 * @param vaddr the first byte of virtual memory to write.
+	 * @param data the array containing the data to transfer.
+	 * @param offset the first byte to transfer from the array.
+	 * @param length the number of bytes to transfer from the array to virtual
+	 * memory.
+	 * @return the number of bytes successfully transferred.
+	 */
+	public int writeVirtualMemory(int vaddr, byte[] data, int offset, int length) {
+/*
+		Lib.assertTrue(offset >= 0 && length >= 0
+				&& offset + length <= data.length);
+*/
+    //checking for valid virtual page conditions
+    if (!(offset >= 0 && length >= 0 && offset + length <= data.length) ||
+        data==null ||
+        vaddr<0 ||
+        vaddr+length>pageTable.length*pageSize) {
+      return -1;
+    }
+
+		byte[] memory = Machine.processor().getMemory();
+
+		// for now, just assume that virtual addresses equal physical addresses
+		if (vaddr < 0 || vaddr >= memory.length)
+			return 0;
+
+		int amount = Math.min(length, memory.length - vaddr);
+    //System.out.println(pageTable[vaddr/pageSize].ppn + (vaddr%pageSize));
+		//System.arraycopy(data, offset, memory, vaddr, amount);
+
+    int iterable = 0;
+    int page, diff;
+    do
+    {
+
+      page = (vaddr + iterable) / pageSize; //getting virtual page
+      diff = (vaddr + iterable) % pageSize; //getting offset within page
+      
+      int spaceLeft = pageSize - diff; //the amount of space we have left to write to.
+      
+      //is the case where we have too much in our length to write
+      if(spaceLeft < length - iterable) //if we have to write more than we have space in the page for
+
+      {
+        //System.out.println(iterable + " " + pageSize + " " + spaceLeft + " " + (length - iterable));
+        System.arraycopy(data, offset+iterable, memory, pageTable[page].ppn*pageSize + diff, spaceLeft);
+        iterable += spaceLeft;
+      }
+      else //is the case where we can write all that's left within the page
+      {
+        System.arraycopy(data, offset+iterable, memory, pageTable[page].ppn*pageSize + diff, length - iterable);
+        iterable += length - iterable;
+      }
+    }
+    while(iterable < length);
+
+		return iterable;
+	}
 
   private int[] CoffSections;
   
